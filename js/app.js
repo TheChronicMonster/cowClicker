cd5f4b63610e73194ac5d2897d6c3ba3b8b0e89e
TheChronicMonster <legendzing@gmail.com>
2015-06-28 00:15:11 -0500

    feat: added pig with function
    Added pig with pig counter function and table.

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ function cowClicker() {
 		document.getElementById("cowNumber").innerHTML = counter;
 	}
 }
+var pigCounter = 0;
+function pigClicker() {
+	if (true) {
+		pigCounter += 1;
+		console.log(pigCounter);
+		document.getElementById("pigNumber").innerHTML = pigCounter;
+	}
+}
